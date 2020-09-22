bc9b2ce0648eb4afebb916816e276a10869735a0
Unknown <davideamabile81@gmail.com>
2020-09-22 16:54:15 +0200

    chiamata ajax

--- js/script.js
+++ js/script.js
@@ -9,17 +9,28 @@ $(document).ready(function(){
       {
       "url": "https://api.themoviedb.org/3/search/movie?api_key=d8103ee9346cae884496275cd6ea3a72",
       "data":{
-        "title": titolo
+        "query": titolo,
+
       },
       "method": "GET",
       "success": function (data) {
-         console.log(data.results);
+        var results = data.results;
+
+        // ciclo per le proprietà
+        for ( i = 0; i < results.length; i++) {
+          console.log(results[i].title);
+          console.log(results[i].original_language);
+          console.log(results[i].vote_average);
+
+        }
+        // fine ciclo
       },
       "error": function () {
       alert("E' avvenuto un errore. " );
       }
     }
       );
+      // fine ajax
   });
   // fine evento
 });
