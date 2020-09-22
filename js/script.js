8ddeede30e8486a2162f1831d8ea78bdfe66ee76
Unknown <davideamabile81@gmail.com>
2020-09-22 21:30:26 +0200

    evnto click, keypress con funzione

--- js/script.js
+++ js/script.js
@@ -2,46 +2,68 @@ $(document).ready(function(){
 
   // creare evento click sul bottone
   $(document).on("click", "#btn", function(){
+    // prendo il titolo dall'input
     var titolo = $("#search").val();
 
-    // effettuare chiamata ajax
-    $.ajax(
-      {
-      "url": "https://api.themoviedb.org/3/search/movie?api_key=d8103ee9346cae884496275cd6ea3a72",
-      "data":{
-        "query": titolo,
-
-      },
-      "method": "GET",
-      "success": function (data) {
-
-        var source = $("#film-template").html();
-        var template = Handlebars.compile(source);
-
-        var results = data.results;
-        // ciclo per le proprietà
-        for ( i = 0; i < results.length; i++) {
-          var name= results[i].title;
-          var originalLanguage = results[i].original_language;
-          var vote = results[i].vote_average;
-
-          var context = {
-          "name": name,
-          "lingua": originalLanguage,
-          "voto": vote
-          };
-          var html = template(context);
-
-          $(".lista_film").append(html);
-        }
-        // fine ciclo
-      },
-      "error": function () {
-      alert("E' avvenuto un errore. " );
-      }
-     }
-    );
-
+    // invocare funzione
+    getTheMovie(titolo);
 });
+// fine evento click
+
+
+// creare evento keypress sull'input
+$("#search").keypress(
+  function(evento){
+   if (evento.which == 13){
+     // condizione dell'inpiut senza testo
+     var titolo = $("#search").val();
+     if ( titolo != "") {
+       // invoco funzione
+       getTheMovie(titolo);
+     }
+   }
+});
+// fine vento keypress
 });
 // fine jquery
+
+
+// funzione chiamata e template
+function getTheMovie(titolo){
+  // effettuare chiamata ajax
+  $.ajax(
+    {
+    "url": "https://api.themoviedb.org/3/search/movie?api_key=d8103ee9346cae884496275cd6ea3a72",
+    "data":{
+      "query": titolo,
+
+    },
+    "method": "GET",
+    "success": function (data) {
+
+      var source = $("#film-template").html();
+      var template = Handlebars.compile(source);
+
+      var results = data.results;
+      // ciclo per le proprietà
+      for ( i = 0; i < results.length; i++) {
+
+        // template
+        var context = {
+        "name": results[i].title,
+        "lingua": results[i].original_language,
+        "vote": results[i].vote_average
+        };
+        var html = template(context);
+
+        $(".lista_film").append(html);
+      }
+      // fine ciclo
+    },
+    "error": function () {
+    alert("E' avvenuto un errore. " );
+    }
+   }
+  );
+  // fine ajax
+}
